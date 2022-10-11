b80ee5bfee2ae471eab390d6a540f0402992c2a4
Jonas Agnezi <jonas.agnezi@gmail.com>
2022-10-11 18:19:12 -0300

    Initial Commit

---
+++ HowManyTimes/HowManyTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowManyTimesApp.swift
+//  HowManyTimes
+//
+//  Created by Jonas Agnezi on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HowManyTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
